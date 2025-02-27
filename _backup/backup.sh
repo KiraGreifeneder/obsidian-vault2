d01c1d31f6bc481a4bb6800659f4d81dc58846bb
KiraDesktop <kira.greifneder@outlook.com>
2025-02-27 17:11:02 +0100

    automated backup

--- _backup/backup.sh
+++ _backup/backup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+LOG_PATH="/home/kira/Documents/obsidian-vault/_backup/"
+
+LOG_FILE="${LOG_PATH}backup_$(date +'%Y%m%d_%H%M%S').log"
+exec &> >(tee -a "$LOG_FILE")
 
 dunstify "Obsidian vault is being pushed to GitHub... Any performance issues should pass shortly."
-git add . > log.txt
-git commit -m "automated backup" > log.txt
-git lfs migrate import --include="*.pdf" > log.txt
-git push --force > log.txt
-git lfs migrate export --everything --include="*" > log.txt
+git add . &> log.txt
+git commit -m "automated backup" --author="KiraDesktop <kira.greifneder@outlook.com>" &> log.txt
+git lfs migrate import --include="*.pdf" &> log.txt
+git push --force &> log.txt
+git lfs migrate export --everything --include="*" &> log.txt
