a2616cb776d47f2cb5f57f16c18f0bcd1d7f2826
KiraDesktop <kira.greifeneder@outlook.com>
2025-02-27 16:49:14 +0100

    automated backup

--- backup.sh
+++ backup.sh
@@ -3,6 +3,6 @@
 dunstify "Obsidian vault is being pushed to GitHub... Any performance issues should pass shortly."
 git add .
 git commit -m "automated backup"
-git lfs import
+git lfs migrate import
 git push --force
 git lfs migrate export --everything --include="*"
