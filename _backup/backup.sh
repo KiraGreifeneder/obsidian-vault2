bbd7eb3ffe93f1fa30708e69686ffe04d71b5571
KiraDesktop <kira.greifeneder@gmail.com>
2025-02-27 17:37:20 +0100

    automated backup

--- _backup/backup.sh
+++ _backup/backup.sh
@@ -6,9 +6,9 @@ LOG_FILE="${LOG_PATH}/backup_$(date +'%Y%m%d_%H%M%S').log"
 touch "$LOG_FILE"
 
 dunstify "Obsidian vault is being migrated and pushed to GitHub..."
-git add . &>> $LOG_FILE
-git commit -m "automated backup" --author="KiraDesktop <kira.greifeneder@gmail.com>" &>> $LOG_FILE
-yes | git lfs migrate import --include="*.pdf" &>> $LOG_FILE
-git push --force &>> $LOG_FILE
-yes | git lfs migrate export --everything --include="*" &>> $LOG_FILE
+git add . >> $LOG_FILE 2>&1
+git commit -m "automated backup" --author="KiraDesktop <kira.greifeneder@gmail.com>" >> $LOG_FILE 2>&1
+yes | git lfs migrate import --include="*.pdf" >> $LOG_FILE 2>&1
+git push --force >> $LOG_FILE 2>&1
+yes | git lfs migrate export --everything --include="*" >> $LOG_FILE 2>&1
 dunstify "Finished migrating and pushing vault to GitHub."
